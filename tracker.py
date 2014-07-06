9f574922c3e1450160d5f25489566c373856ac90
Luca <luca.lanziani@gmail.com>
2014-07-06 12:18:52 +0100

    Revert "Add thread"
    This reverts commit e7e8f23b89ba7f7913cb2431e62c49926bba91c6.

--- tracker.py
+++ tracker.py
@@ -3,33 +3,19 @@ import sublime_plugin
 import urllib2
 import json
 import time
-import threading
-import Queue
-
 
 SETTINGS_FILE = 'Trackit.sublime-settings'
-CommunicationQueue = Queue.Queue()
 
-def send_data(filename, key, timestamp, url):
+
+def send_data(filename, key, url):
     try:
-        data = json.dumps({"timestamp": timestamp, "filepath": filename, "key": key})
+        data = json.dumps({"timestamp": time.time(), "filepath": filename, "key": key})
         clen = len(data)
         req = urllib2.Request(url, data, {'Content-Type': 'application/json', 'Content-Length': clen})
-        urllib2.urlopen(req, timeout=0.01)
+        f = urllib2.urlopen(req, timeout=0.01)
+        print f.read()
     except Exception as e:
-        sublime.status_message("%s while contacting %s" % (e, url))
-
-
-class DevStatsSender(threading.Thread):
-
-    def __init__(self, *args, **kwds):
-        super(DevStatsSender, self).__init__(*args, **kwds)
-
-    def run(self):
-        msg = CommunicationQueue.get()
-        while msg is not None:
-            send_data(msg['filename'], msg['key'], msg['timestamp'], msg['url'])
-            msg = CommunicationQueue.get()
+        print "%s while contacting %s" % (e, url)
 
 
 class TrackitCommand(sublime_plugin.TextCommand):
@@ -39,19 +25,9 @@ class TrackitCommand(sublime_plugin.TextCommand):
         endpoint = settings.get('endpoint')
         on_keypress = settings.get('on_keypress')
         filename = self.view.file_name()
-        msg = {
-            'filename': filename,
-            'key': key,
-            'timestamp': time.time(),
-            'url': endpoint + on_keypress
-        }
-        CommunicationQueue.put(msg)
+        send_data(filename, key, endpoint + on_keypress)
         if command is not None:
             self.view.run_command(command, args)
         else:
             for pos in self.view.sel():
-                if pos.size() > 1:
-                    self.view.erase(edit, pos)
                 self.view.insert(edit, pos.begin(), key)
-
-DevStatsSender().start()
